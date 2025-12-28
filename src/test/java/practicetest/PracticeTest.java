d05a51720cff5086729d7ad02ec8f0550b074016
tusharjagdanevlogs <tusharjagdane@gmail.com>
2025-12-28 16:21:42 +0530

    Creating WF

--- src/test/java/practicetest/PracticeTest.java
+++ src/test/java/practicetest/PracticeTest.java
@@ -1,5 +1,5 @@
 package practicetest;
-//this is hb
+//Creating WF with this code commit push and creating WF
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
